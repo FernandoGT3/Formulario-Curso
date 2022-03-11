2b5f044576e2e5c3bb50720c6f857b4221deeb97
Luiz Moloni <luiz.moloni@gmail.com>
2022-03-11 19:54:47 -0300

    Validando Usuário
     Adicionei uma condição no método checkFields relacionada a validação do usuário.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,33 +15,59 @@ class ValidateForm {
         //console.log('Não enviou');
 
         this.checkFields();
+        this.checkPasswords();
     }
 
-    checkFields(){
+    checkFields() {
         let valid = true;
 
-        for(let errorText of this.form.querySelectorAll('.error-text')){
+        for (let errorText of this.form.querySelectorAll('.error-text')) {
             errorText.remove();
         }
 
-        for(let field of this.form.querySelectorAll('.validate')){
-            if(!field.value){
+        for (let field of this.form.querySelectorAll('.validate')) {
+            if (!field.value) {
                 const message = `Campo "${field.previousElementSibling.innerHTML}" não pode estar em branco`;
                 this.createError(field, message);
                 valid = false;
             }
             //console.log(valid);
 
-            if(field.classList.contains('cpf')){
-                if(!this.validationCPF(field)) valid = false;
+            if (field.classList.contains('cpf')) {
+                if (!this.validationCPF(field)) valid = false;
+            }
+
+
+            if (field.classList.contains('user')) {
+                if (!this.validationUser(field)) valid = false;
             }
         }
+
+        return valid;
     }
 
-    validationCPF(field){
+    validationUser(field) {
+        const user = field.value;
+        let valid = true;
+
+        if (user.length < 3 || user.length > 12) {
+            const message = 'Usuário Inválido, deve conter entre 3 e 12 caracteres'
+            this.createError(field, message);
+            valid = false;
+        }
+
+        if(!user.match(/^[a-zA-Z0-9]+$/g)){
+            const message = 'Usuário Inválido, deve conter apenas letras e/ou números'
+            this.createError(field, message);
+            valid = false;
+        }
+        return valid;
+    }
+
+    validationCPF(field) {
         const cpf = new ValidaCPF(field.value);
 
-        if(!cpf.valida()){
+        if (!cpf.valida()) {
             const message = 'CPF Inválido'
             this.createError(field, message);
             return false;
@@ -49,7 +75,11 @@ class ValidateForm {
         return true;
     }
 
-    createError(field, message){
+    checkPasswords(){
+        
+    }
+
+    createError(field, message) {
         const div = document.createElement('div');
         div.innerHTML = message;
         div.classList.add('error-text');
